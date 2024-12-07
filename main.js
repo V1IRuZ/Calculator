e5a9e43eb2855fb763962573a1c1181b29886111
Esa <esa-pitkanen@hotmail.com>
2024-12-07 18:33:35 +0200

    Add functions for basic math operators
    Added operations for addition, subtraction, multiplication, and division, which are essential functions in a calculator.

---
+++ main.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const test = divide(6, 2);
+console.log(test); 
