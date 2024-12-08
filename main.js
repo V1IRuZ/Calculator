14bf0e22c97b92b64ab951de42a83c8599889e1b
Esa <esa-pitkanen@hotmail.com>
2024-12-08 15:04:41 +0200

    Define the calculation procedure
    A function was created that completes the calculation operation based on the selected operator.
    
    Buttons for numbers and operators were created in the HTML file. As well as the display where the input is displayed to the user.

--- main.js
+++ main.js
@@ -3,5 +3,22 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const test = divide(6, 2);
+let firstNumber = 0
+let operator = "*"
+let secondNumber = 0
+
+function operate() {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return substract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
+
+const test = operate()
 console.log(test); 
