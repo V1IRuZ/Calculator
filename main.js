43bfbe48ec4fb7ab0bd07d41beedaf5216a83ec7
Esa <esa-pitkanen@hotmail.com>
2024-12-14 16:41:52 +0200

    Add another number and the calculation
    A second number function was created that has its own rules. When the second number is stored in the variable and the user presses the equal button, the program gives the result according to the selected calculation formula.

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const display = document.querySelector(".display");
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const operators = ["+", "-", "*", "/"];
 
-let firstNumber = ""
-let operator = ""
-let secondNumber = ""
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let calculate = false;
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
     let pressedButton = button.id;
+    
 
         if (pressedButton === "clear") {
             firstNumber = "";
@@ -18,22 +20,34 @@ buttons.forEach((button) => {
             operator = "";
             display.textContent = "";
         }
+      
         if (!operator) {
             getFirstNumber(pressedButton);
-        } 
+        } else {
+            getSecondNumber(pressedButton);
+        }
 
+        if (calculate === true){
+            operate(firstNumber, operator, secondNumber);
+            calculate = false;
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+              
+        }
+        console.log(calculate);
     })
 })
 
 function getFirstNumber(selectedButton) {
+    // RULES
     // checking number is selected
     const isNumber = numbers.includes(selectedButton);
-    // checking if first character in string is number
+    // checking if first character in first number string is number
     const firstCharacterIsNumber = numbers.includes(firstNumber[0]);
     // checking if operator is selected
     const isOperator = operators.includes(selectedButton);
-
-
+    
     if (firstNumber === "" && selectedButton === "0") {
         display.textContent = `${firstNumber}`;
     
@@ -51,6 +65,33 @@ function getFirstNumber(selectedButton) {
     }
 }
 
+function getSecondNumber (selectedButton) {
+    // RULES
+    // checking number is selected
+    const isNumber = numbers.includes(selectedButton);
+    // checking if first character in second number string is number
+    const firstCharacterIsNumber = numbers.includes(secondNumber[0]);
+    // checking if operator is selected
+    const isOperator = operators.includes(selectedButton);
+
+    if (secondNumber === "" && selectedButton === "0") {
+        display.textContent = `${firstNumber} ${operator}`;
+        
+    
+    } else if (firstCharacterIsNumber && selectedButton === "=") {
+        calculate = true;
+        display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+        return secondNumber, calculate;
+
+    } else if (isNumber) {
+            secondNumber += selectedButton;
+            display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    
+    } else {
+        display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    }
+}
+
 
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
@@ -62,14 +103,25 @@ const divide = (a, b) => a / b;
 function operate(firstNumber, operator, secondNumber) {
     firstNumber = parseInt(firstNumber);
     secondNumber = parseInt(secondNumber);
+    let result = 0
     if (operator === "+") {
-        return add(firstNumber, secondNumber);
+        result = add(firstNumber, secondNumber);
+        display.textContent = `${result}`;
+        return result;
     } else if (operator === "-") {
-        return substract(firstNumber, secondNumber);
+        result = substract(firstNumber, secondNumber);
+        display.textContent = `${result}`;
+        return result;
     } else if (operator === "*") {
-        return multiply(firstNumber, secondNumber);
+        result = multiply(firstNumber, secondNumber);
+        display.textContent = `${result}`;
+        return result;
     } else if (operator === "/") {
-        return divide(firstNumber, secondNumber);
+        result = divide(firstNumber, secondNumber);
+        display.textContent = `${result}`;
+        return result;
     }
+
+
 }
 
